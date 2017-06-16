490fa7dd91f859da80ec4273905d9afcc9e60156
matthewspydell <matthew.spydell@colorado.edu>
2017-06-16 15:46:33 -0600

    Added the ability to pass a delay value into the stream scripts

--- lunar/joystick/inputTest.py
+++ lunar/joystick/inputTest.py
@@ -1,11 +1,28 @@
-from inputs import devices
-from inputs import get_gamepad
+import pygame
 
-for device in devices:
-	print(device)
+xboxController = pygame.joystick.Joystick(0)
+xboxController.init()
+print(xboxController.get_name())
+print("Axes", (xboxController.get_numaxes()))
+print("Balls", xboxController.get_numballs())
+print("Buttons", xboxController.get_numbuttons())
+print("Hats", xboxController.get_numhats())
+if xboxController.get_init() is True:
+	print("Initialized properly")
 
-while 1:
-	events = get_gamepad()
-	print("hello")
-	for event in events:
-		print(event.ev_type, event.code, event.state)
+while not found:
+    trial += 1
+    print(trial)
+    pygame.event.pump()
+    for a in range(0, xboxController.get_numaxes()):
+        if round(xboxController.get_axis(a) * 100) != 0:
+            print(xboxController.get_axis(a))
+    for b in range(0, xboxController.get_numballs()):
+        if xboxController.get_ball(b) != 0: 
+            print(xboxController.get_ball(b))
+    for c in range(0, xboxController.get_numbuttons()):
+        if xboxController.get_button(c) is False: 
+            print(xboxController.get_button(button(c)))
+    for d in range(0, xboxController.get_numhats()):
+        if xboxController.get_hat(d) != (0, 0): 
+            print(xboxController.get_hat(d))
--- lunar/topcatStream.sh
+++ lunar/topcatStream.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-nc -l 5001 | delay 2300 | mplayer -fps 200 -demuxer h264es -
+if [ $1 = "" ]; then
+	echo "Usage: ./topcatStream.sh [delay in milliseconds]"
+	exit 0
+fi
+
+echo "Listening on port 5001"
+
+nc -l 5001 | delay $1 | mplayer -fps 200 -demuxer h264es -
