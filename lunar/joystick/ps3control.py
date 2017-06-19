25695551d86df53647f53ee10acde51757dc242f
matthewspydell <matthew.spydell@colorado.edu>
2017-06-19 08:19:49 -0600

    Added a python script that allows control of the rover through a Playstation 3 controller

---
+++ lunar/joystick/ps3control.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import serial
+import pygame
+import sys
+import os
+
+
+
+def convert_range(raw):
+	# this is an artifact of how we sent values with the analog joystick, easiest to just not change
+	return ((raw * 2042) + 2042)
+
+pygame.init()
+pygame.joystick.init()
+cmdjoystick = pygame.joystick.Joystick(0)
+cmdjoystick.init()
+ser = serial.Serial('/dev/ttyUSB0', timeout=1)
+os.system('clear')
+
+while(1):
+	event = pygame.event.poll()
+	# print(str(convert_range(cmdjoystick.get_axis(1))))
+	# print(str(convert_range(cmdjoystick.get_axis(0))))
+	# print(str(convert_range(cmdjoystick.get_axis(3))))
+	# print(str(convert_range(cmdjoystick.get_axis(2))))
+	if (cmdjoystick.get_button(2) == 1):
+		ser.write("c 0 0 0\n")  # center camera command
+
+	# this is negative because this and the analog joystick have opposite directions on that one axis
+	ser.write(str(convert_range(cmdjoystick.get_axis(1))))
+	ser.write(" ")
+	ser.write(str(convert_range(cmdjoystick.get_axis(0))))
+	ser.write(" ")
+	ser.write(str(convert_range(-cmdjoystick.get_axis(3))))
+	ser.write(" ")
+	ser.write(str(convert_range(cmdjoystick.get_axis(2))))
+	ser.write("\n")
+
+	response = ser.readline()
+	# the response is as so: xangle yangle latdeg latmin latfracmin latfracmin latdir longdeg longmin
+	# longfracmin longfracmin longdir altitude time \n
+	reslist = response.split(" ")
+	if (reslist != [""]):
+		pitch = reslist[0]
+		roll = reslist[1].strip()
+		print("Pitch: " + str('%g' % round(float(pitch))) + u'\u00b0' + "                    ", end='\n')
+		print("Roll: " + str('%g' % round(float(roll))) + u'\u00b0' + "                  ", end='\033[1A' + '\r')
+		sys.stdout.flush()
